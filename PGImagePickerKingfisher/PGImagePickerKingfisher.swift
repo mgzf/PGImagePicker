8cfb4fe85036e84a328249623eabea441d78acfa
muyexi <muyexi@gmail.com>
2017-12-18 18:00:42 +0800

    feat(PGImagePickerKingfisher): hide statusBar

--- PGImagePickerKingfisher/PGImagePickerKingfisher.swift
+++ PGImagePickerKingfisher/PGImagePickerKingfisher.swift
@@ -17,6 +17,18 @@ public class PGImagePickerKingfisher: PGImagePicker {
     public var placeholder: UIImage?
     public var imageUrls: [String]?
     
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.isStatusBarHidden = true
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        UIApplication.shared.isStatusBarHidden = false
+    }
+    
     override public func setupImageView(cell: PGCollectionViewCell, indexPath: IndexPath, imageView: UIImageView){
         if imageUrls != nil && indexPath.row < (imageUrls?.count)! {
             let cellImageView: UIImageView =  cell.scrollView.imageView
